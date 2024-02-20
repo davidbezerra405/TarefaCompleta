adc48c85e37bffa72babf44d17b3b4d95b8e29fe
DAVID DE ALMEIDA BEZERRA <68335111+davidbezerra405@users.noreply.github.com>
2024-02-20 16:11:48 -0300

    termino

--- app/src/main/java/com/example/tarefacompleta/MainActivity.kt
+++ app/src/main/java/com/example/tarefacompleta/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.tarefacompleta
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tarefacompleta.ui.theme.TarefaCompletaTheme
 
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +36,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    val image = painterResource(R.drawable.ic_task_completed)
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(modifier = Modifier.align(Alignment.Center)) {
+            Image(
+                painter = image,
+                contentDescription = null
+            )
+            Text(
+                text = stringResource(R.string.txt_tarefa_concluida),
+                modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+            )
+            Text(
+                text = stringResource(R.string.txt_bom_trabalho),
+                modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TarefaCompletaTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
